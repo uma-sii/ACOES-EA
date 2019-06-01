805b95c5763bc83ccbc5df1856fea756da7ae1bd
Manuel <Manuel@LAPTOP-NI8PKI9P>
2019-06-01 20:09:02 +0200

    Merge origin/master
    Conflicts:
    	ACOES/web/registration.xhtml

--- ACOES/src/java/org/acoes/view/validator/EmailValidator.java
+++ ACOES/src/java/org/acoes/view/validator/EmailValidator.java
@@ -26,9 +26,6 @@ public class EmailValidator implements Validator{
 	String email = (String) value;
         String emailRegex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
         
-        if(email.equals(currentSession.getUser().getEmail()))
-            return;
-        
         if(usersService.doesUserExist(email)){
             createFacesMessage(context, "Error: User already exists", "Error: User already exists");
         }
